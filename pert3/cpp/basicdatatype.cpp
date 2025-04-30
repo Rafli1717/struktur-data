4fd0226f4f5c3217fd4f0a461df3f1e580553aa0
root <root@RAFLI-E191295.>
2025-04-30 14:10:49 +0700

    catatan pert 4

---
+++ pert3/cpp/basicdatatype.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main (){
+    int a = 10;
+    float b = 3.14;
+    double c = 9.00;
+    char d '0';
+    bool e = true;
+    string f = "hello";
+
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    cout << d << enld;
+    cout << e << enld;
+}
